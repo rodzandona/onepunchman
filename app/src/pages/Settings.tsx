ac20f225978d673011de9a0608c4d89bf16fee05
LeticiaCarvalho-acti <leticia.carvalho.acti@gmail.com>
2025-12-09 16:36:28 -0300

    page configurações pronta

--- app/src/pages/Settings.tsx
+++ app/src/pages/Settings.tsx
@@ -1,18 +1,57 @@
 "use client";
 
+
+
 import { Card } from "@/components/ui/card";
+import { Icon } from "@iconify/react";
+import { Button } from "@/components/ui/button";
+
 
 export default function Settings() {
 
 
   return (
-    <div className="">
-      <Card
-      className="bg-red-500 w-40 h-40 rounded-sm"
-      > teste </Card>
-      <div className="bg-gray-100 w-full">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">.</h1>
-     
+    <div className="font-thin p-[6px]">
+
+      <div className="bg-gray-100 w-full p-5 shadow ">
+        <div className="flex justify-start gap-10">
+          <Icon icon="solar:arrow-left-linear" className=" mt-1 h-8 w-8 text-center hover:bg-gray-300 hover:rounded-full" />
+          <div className="flex gap-2">
+            <Icon icon="solar:settings-line-duotone" className="h-7 w-7 text-gray-500 mt-1" />
+            <h1 className="text-2xl  font-bold text-gray-800">Configurações</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-[6px]">
+        <div className="flex justify-center text-center">
+          <img
+            src="johnson-johnson-logo-png_seeklogo-500414-removebg-preview.png"
+            alt="imagem j&j centro"
+            className="w-40 mx-auto"
+          />
+        </div>
+
+        <Card className="rounded-lg p-[8px] shadow-lg mb-6">
+          <div className="flex justify-start gap-3 p-[2px] ">
+            <Icon icon="solar:user-circle-bold-duotone" className="w-8 h-8 text-red-500" />
+            <div className="flex items-center justify-between w-full">
+              <h1 className="text-lg font-normal">Admin_jsj_098</h1>
+              <Button className="bg-red-700 hover:bg-red-900 shadow-lg">
+                Sair
+              </Button>
+            </div>
+          </div>
+        </Card>
+        <div className="w-full h-px bg-gray-500 mb-4"></div>
+
+        <Card className="w-full rounded-lg shadow-lg">
+          <div className="justify-end p-2 font-semibold">
+            <div className="bg-gray-200 rounded-lg p-2">Versão do App </div>
+            <div className="p-2">Versão de Compilação</div>
+          </div>
+        </Card>
+
       </div>
     </div>
   );
