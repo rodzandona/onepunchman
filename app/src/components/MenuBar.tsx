beea03484ce80cfc1eb641bc7c9e0ecb0754f253
Leonardo da Silva Irineu <leonardo1692004@gmail.com>
2025-12-10 10:38:00 -0300

    fix: definindo tamanho do menubar para card home se adequar

--- app/src/components/MenuBar.tsx
+++ app/src/components/MenuBar.tsx
@@ -11,44 +11,39 @@ export default function MenuBar({ activeTab, onTabChange }: MenuBarProps) {
   `;
 
   return (
-    <nav
-      className="fixed bottom-0 left-0 w-full z-50 border-t border-gray-200 py-3 bg-white sm:border-0 sm:py-0"
-    >
+    <nav id="main-footer" className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-white rounded-2xl p-3 shadow-xl z-50 w-[calc(100%-2rem)] max-w-md">
+
       <div
         className="flex justify-center"
       >
         <div
-          className="flex gap-2 p-2 bg-gray-200/80 rounded-2xl shadow-lg w-full max-w-xl mx-auto sm:mb-6 sm:w-full sm:px-2 sm:py-2 sm:rounded-2xl md:w-[420px]"
+          className="flex gap-2 p-2 bg-gray-200/80 rounded-2xl shadow-lg w-full max-w-xl mx-auto sm:w-full sm:px-2 py-2 sm:rounded-2xl md:w-[420px]"
         >
           <button
             onClick={() => onTabChange('home')}
-            className={`${baseButtonClasses} transition-all duration-300 will-change-transform ${
-              activeTab === 'home'
-                  ? 'bg-white text-[#D82B14] shadow-[1px_1px_1px_rgba(0,0,0,0.10)] -translate-y-1 border-1 border-[#D82B14]'
+            className={`${baseButtonClasses} transition-all duration-300 will-change-transform ${activeTab === 'home'
+                ? 'bg-white text-[#D82B14] shadow-[1px_1px_1px_rgba(0,0,0,0.10)] -translate-y-1 border-1 border-[#D82B14]'
                 : 'text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:shadow-sm border border-gray-200 translate-y-0'
-            } sm:py-2 sm:px-0`}
+              } sm:py-2 sm:px-0`}
           >
             <Icon
               icon="solar:home-angle-linear"
-              className={`w-6 h-6 mb-1 transition-all duration-300 ${
-                activeTab === 'home' ? 'text-[#D82B14] scale-110' : 'text-gray-400 scale-100'
-              }`}
+              className={`w-6 h-6 mb-1 transition-all duration-300 ${activeTab === 'home' ? 'text-[#D82B14] scale-110' : 'text-gray-400 scale-100'
+                }`}
             />
             <span className={`text-xs font-semibold transition-all duration-300 ${activeTab === 'home' ? 'text-[#D82B14]' : ''}`}>Início</span>
           </button>
           <button
             onClick={() => onTabChange('settings')}
-            className={`${baseButtonClasses} transition-all duration-300 will-change-transform ${
-              activeTab === 'settings'
-                  ? 'bg-white text-[#D82B14] shadow-[0_4px_12px_rgba(0,0,0,0.08)] -translate-y-1 border-1 border-[#D82B14]'
+            className={`${baseButtonClasses} transition-all duration-300 will-change-transform ${activeTab === 'settings'
+                ? 'bg-white text-[#D82B14] shadow-[0_4px_12px_rgba(0,0,0,0.08)] -translate-y-1 border-1 border-[#D82B14]'
                 : 'text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:shadow-sm border border-gray-200 translate-y-0'
-            } sm:py-2 sm:px-0`}
+              } sm:py-2 sm:px-0`}
           >
             <Icon
               icon="solar:settings-linear"
-              className={`w-6 h-6 mb-1 transition-all duration-300 ${
-                activeTab === 'settings' ? 'text-[#D82B14] scale-110' : 'text-gray-400 scale-100'
-              }`}
+              className={`w-6 h-6 mb-1 transition-all duration-300 ${activeTab === 'settings' ? 'text-[#D82B14] scale-110' : 'text-gray-400 scale-100'
+                }`}
             />
             <span className={`text-xs font-semibold transition-all duration-300 ${activeTab === 'settings' ? 'text-[#D82B14]' : ''}`}>Config</span>
           </button>
