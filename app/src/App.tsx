8a36fcbbd10060982b2c3f29b959ad0271ee2493
LeticiaCarvalho-acti <leticia.carvalho.acti@gmail.com>
2025-12-12 13:19:06 -0300

    feat: versão da aplicação no footer

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,28 +2,16 @@ import { useState } from 'react'
 import MenuBar from './components/MenuBar'
 import Home from './pages/Home'
 import Settings from './pages/Settings'
-import { Toaster, toast } from "sonner";
-import Login from './pages/Login';
-
-
+import Login from './pages/Login'
+import { Toaster } from "sonner";
 
 function App() {
-  const [activeTab, setActiveTab] = useState<'home' | 'settings'>('home')
+  const [activeTab, setActiveTab] = useState<'home' | 'settings' | 'login'>('home')
 
   return (
-  //   <div className="h-screen flex flex-col bg-gray-50">
-  //     {/* Conteúdo principal */}
-  //     <main className="flex-1 overflow-y-auto pb-28">
-  //       {activeTab === 'home' ? <Home /> : <Settings />}
-  //     </main>
-      
-  //     {/* MenuBar fixo na parte inferior */}
-  //     <MenuBar activeTab={activeTab} onTabChange={setActiveTab} />
-  //     <Toaster richColors />
-  //   </div>
-  <Login/>
+
+    <Login/>
   )
-  
 }
 
 export default App
